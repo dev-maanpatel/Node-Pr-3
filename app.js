f9133fe47fb6846d934a631977bd6fdfc58858f5
dev-maanpatel <dev.maanpatel1@gmail.com>
2026-06-08 10:21:19 +0530

    Update project UI and fix minor issues

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join("views"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join("public")));
 
 app.get("/", (req, res) => {
     res.render("pages/home", { title: "TransitFlow | Home" });
